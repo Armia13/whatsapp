37445290a3d6410df2f8e1124e8627001e970975
Armia Afridi <49521014+Armia13@users.noreply.github.com>
2019-08-31 09:07:36 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,44 +1,46 @@
-let handleSuccess = function (response) {
-  // handle success
-  console.log('Tired')
-  console.log(response)
-  for (let i = 0; i < response.data.length; i++) {
-    // console.log(response.data[i]);
-    let messages = response.data[i]
 
-    document.querySelector('ul').innerHTML += `
-      <li>
-       <div class="chat-box">
-         <img src=${messages.profilePic}>
-         <div class="msg"><h4>${messages.name}</h4>
-           <p class="num">${messages.number}</p> 
-           <p class="first-line">${messages.firstLine}</p>
-         </div>
-         <div class="dt">
-           <p class="msg-time">${messages.time}</p>
-           <p class="unread">${messages.numbUnread}</p> 
-         </div>
-        </div> 
-      </li>`
-    if (response.data.numbUnread > 0) {
-      document.getElementsByClassName('chat-box').style.backgroundColor = 'red'
+let hours = 0
+let minutes = 0
+let seconds = 0
+let timer
+let isRunning = false
+
+function updateTime () {
+  if (seconds == 59) {
+    seconds = 0
+    if (minutes === 59) {
+      minutes = 0
+      hours++
+    } else {
+      minutes++
     }
+  } else {
+    seconds++
   }
-
-  // document.querySelector("ul").innerHTML=
+  document.getElementById('seconds').innerHTML = seconds;
+  document.getElementById('minutes').innerHTML = minutes;
+  document.getElementById('hours').innerHTML = hours;
 }
 
-axios
-  .get('https://tk-whatsapp.herokuapp.com/messages')
-  .then(handleSuccess)
-  // handle success is a callback function
-  // jab request ayegi to then chalega
-  .catch(function (error) {
-    // handle error
-    console.log('Rttot')
-    console.log(error)
-  })
-  .finally(function () {
-    // always executed
-    console.log('The end')
-  })
+function startTime () {
+  if (isRunning === false) {
+    timer = setInterval(updateTime, 1000)
+    isRunning = true
+  }
+}
+
+function stopTime () {
+  clearInterval(timer)
+  isRunning = false
+}
+
+function resetTime () {
+  hours = 0
+  minutes = 0
+  seconds = 0
+  document.getElementById('seconds').innerHTML = seconds;
+  document.getElementById('minutes').innerHTML = minutes;
+  document.getElementById('hours').innerHTML = hours;
+  stopTime()
+}
+
